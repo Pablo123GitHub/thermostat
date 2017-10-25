d64bd97313fd2b5bcce38b18b892ed8273efa06e
Pablo <pabloandresvdl@gmail.com>
2017-10-25 15:54:16 +0100

    set powersaving mode and respective maximum temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -20,14 +20,53 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(19);
   });
 
-  it('has a minimum temperature of 10', function(){
-  for (var i = 0; i < 10; i++) {
-    thermostat.downTemperature();
-  }
-    var dropToNine = function () {
+  it('has a minimum temperature of 10', function() {
+    for (var i = 0; i < 10; i++) {
+      thermostat.downTemperature();
+    }
+    var dropToNine = function() {
       thermostat.downTemperature()
     }
     expect(dropToNine).toThrowError("temperature cannot be under 10");
+  });
+
+  it('saving mode set', function() {
+    thermostat.setSavingMode();
+    expect(thermostat.isPowerSavingMode()).toEqual(true)
+  });
+
+  it('does not allow to go beyon the temperature of 25 on powerSavingMode', function() {
+    thermostat.setSavingMode();
+    for (var i = 0; i < 5; i++) {
+      thermostat.upTemperature();
+    };
+    var overTwentyFive = function () {
+      thermostat.upTemperature();
+    };
+    expect(overTwentyFive).toThrowError("temperature cannot go beyond 25 on PowerSavingMode");
+
+  });
+
+
+  it('temperature can be set to 26 when powerSavingMode is OFF', function() {
+    thermostat.setOffSavingMode();
+    for (var i = 0; i < 6; i++) {
+      thermostat.upTemperature();
+    };
+
+    expect(thermostat.temperature).toEqual(26);
+  });
+
+  it('temperature throws an error message when it goes over 32 and powerSavingMode is OFF', function(){
+    thermostat.setOffSavingMode();
+    for (var i = 0; i < 12; i++) {
+      thermostat.upTemperature();
+    };
+    var overThirtyTwo = function() {
+      thermostat.upTemperature();
+    }
+    expect(overThirtyTwo).toThrowError("temperature cannot go over 32 when PowerSavingMode is off");
+
   });
 
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,8 +1,16 @@
 function Thermostat() {
   this.temperature = 20;
+  this.setSavingMode();
+
 }
 
 Thermostat.prototype.upTemperature = function() {
+  if (this.temperature == 25 && this._powerSavingMode == true) {
+    throw new Error("temperature cannot go beyond 25 on PowerSavingMode")
+  }
+  if (this.temperature == 32){
+    throw new Error("temperature cannot go over 32 when PowerSavingMode is off")
+  }
   this.temperature++;
 };
 
@@ -12,3 +20,15 @@ Thermostat.prototype.downTemperature = function() {
   };
   this.temperature--;
 };
+
+Thermostat.prototype.setSavingMode = function () {
+  this._powerSavingMode = true ;
+};
+
+Thermostat.prototype.isPowerSavingMode = function(){
+  return this._powerSavingMode
+};
+
+Thermostat.prototype.setOffSavingMode = function(){
+  this._powerSavingMode = false;
+}
