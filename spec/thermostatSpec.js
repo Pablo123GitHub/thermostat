c1c9412cf240eea32efa8721dc74bd001987c5cc
Pablo <pabloandresvdl@gmail.com>
2017-10-25 11:56:10 +0100

    set default temperature to 20

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,17 @@
+describe("Thermostat", function(){
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it("should start with a default temperature of 20", function(){
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+
+
+
+
+
+})
