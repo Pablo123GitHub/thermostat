2d22bbfb2d3eec8756f4bbcbb0a613107f6192aa
Pablo <pabloandresvdl@gmail.com>
2017-10-25 14:30:30 +0100

    add function to decrement temperature by 1

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,21 +1,34 @@
-describe("Thermostat", function(){
+describe("Thermostat", function() {
   var thermostat;
 
-  beforeEach(function(){
+  beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  it("should start with a default temperature of 20", function(){
+  it("should start with a default temperature of 20", function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
-it("should raise the temperature by 1", function(){
-  thermostat.upTemperature();
-  expect(thermostat.temperature).toEqual(21);
-  
+  it("should raise the temperature by 1", function() {
+    thermostat.upTemperature();
+    expect(thermostat.temperature).toEqual(21);
+
+  });
+
+  it("should decrease the temperature by 1", function() {
+    thermostat.downTemperature();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10', function(){
+  for (var i = 0; i < 10; i++) {
+    thermostat.downTemperature();
+  }
+    var dropToNine = function () {
+      thermostat.downTemperature()
+    }
+    expect(dropToNine).toThrowError("temperature cannot be under 10");
+  });
+
+
 });
-
-
-
-
-})
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,14 @@
-function Thermostat(){
+function Thermostat() {
   this.temperature = 20;
-};
+}
 
 Thermostat.prototype.upTemperature = function() {
   this.temperature++;
-}
+};
+
+Thermostat.prototype.downTemperature = function() {
+  if (this.temperature == 10) {
+    throw new Error("temperature cannot be under 10")
+  };
+  this.temperature--;
+};
