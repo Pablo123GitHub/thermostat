44a7cd0d7468d155c35056cbcf55cb522d9440dc
Pablo <pabloandresvdl@gmail.com>
2017-10-26 20:31:21 +0100

    add form to submit a town and see the city temperature via  weather API

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,64 @@
 $(document).ready(function() {
+  //
+  // $.get('http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=ed09718f109c7e11045775d311bb4c7e&units=metric', function(data){
+  //   console.log(data.main.temp)
+  // });
+  // interface.js, within the $(document).ready(function() { })
+  // $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  //   $('#current-temperature').text(data.main.temp);
+  // })
+
+  // {
+  //  "id": 2643743,
+  //  "name": "London",
+  //  "country": "GB",
+  //  "coord": {
+  //    "lon": -0.12574,
+  // ed09718f109c7e11045775d311bb4c7e
+  //    "lat": 51.50853
+  //  }
 
   thermostat = new Thermostat();
 
   $('#temperature').text(thermostat.DEFAULT_TEMPERATURE);
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=ed09718f109c7e11045775d311bb4c7e';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  }
+  displayWeather('london');
+
+  console.log($('#select-city'))
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
+
+  // $('#current-city').change(function(){
+  //   var city = $('#current-city').val();
+  //   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  //   $('#current-temperature').text(data.main.temp)
+  // });
+
+// var selected_option = $('#current-city option:selected');
+
+// $('#current-city').on('change', function() {
+//   console.log(selected_option);
+//   $('#city-temperature').text(selected_option);
+//   console.log(this.value);
+// })
+
+// $('#current-city option').each(function(){
+//   if ($(this).is(':selected')) console.log($('#current-city option:selected').text());
+
+
 
 
   $("#down").click(function() {
@@ -45,10 +101,6 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 
-});
 
-// $( "a" ).addClass( "test" );
-$("a").click(function(event) {
-  event.preventDefault();
-  $(this).hide("slow");
+
 });
